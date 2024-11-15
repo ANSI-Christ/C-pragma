2e88ced2df3fc2ecd7abc24b56def7d87fd15d59
Klaus <ansichrist@yandex.ru>
2024-11-16 00:08:54 +0300

    peek-peak

--- outmain.h
+++ outmain.h
@@ -14,10 +14,10 @@
 
 #if _OUTMAIN_SYNTAX == 1 /* GCC */
     #ifdef OUTMAIN_BEFORE
-        __attribute__((__constructor__)) static void M_JOIN(_OM_,M_PEEK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
+        __attribute__((__constructor__)) static void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
     #endif 
     #ifdef OUTMAIN_AFTER
-        __attribute__((__destructor__)) static void M_JOIN(_OM_,M_PEEK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
+        __attribute__((__destructor__)) static void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
     #endif 
 #endif
 
@@ -27,25 +27,25 @@
 
 #if _OUTMAIN_SYNTAX == 3 /*  */
     #ifdef OUTMAIN_BEFORE
-        static void M_JOIN(_OM_,M_PEEK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
-        #pragma startup M_JOIN(_OM_,M_PEEK(OUTMAIN_BEFORE))
+        static void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
+        #pragma startup M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))
     #endif 
     #ifdef OUTMAIN_AFTER
-        static void M_JOIN(_OM_,M_PEEK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
-        #pragma end M_JOIN(_OM_,M_PEEK(OUTMAIN_AFTER))
+        static void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
+        #pragma end M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))
     #endif 
 #endif
 
 #if _OUTMAIN_SYNTAX == 4 /*  */
     #ifdef OUTMAIN_BEFORE
-        void M_JOIN(_OM_,M_PEEK(OUTMAIN_BEFORE))(void){
-            __asm__ (".section .init \n call " M_STRING(M_JOIN(_OM_,M_PEEK(OUTMAIN_BEFORE))) " \n .section .text\n");
+        void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){
+            __asm__ (".section .init \n call " M_STRING(M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))) " \n .section .text\n");
             M_REMAINED(OUTMAIN_BEFORE);
         }
     #endif
     #ifdef OUTMAIN_AFTER
-        void M_JOIN(_OM_,M_PEEK(OUTMAIN_AFTER))(void){
-            __asm__ (".section .fini \n call " M_STRING(M_JOIN(_OM_,M_PEEK(OUTMAIN_AFTER))) " \n .section .text\n");
+        void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){
+            __asm__ (".section .fini \n call " M_STRING(M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))) " \n .section .text\n");
             M_REMAINED(OUTMAIN_AFTER);
         }
     #endif 
